152e0fbc23c81821afb9c8ec23b6d26ead6f7e84
Akshaj Verma <akshajverma7@gmail.com>
2020-03-24 19:11:50 +0530

    Added next.config.js to support static builds.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import SideNavbar from "../components/SideNavbar"
 import MainPage from "../components/MainPage"
+import MyImage from "../public/MyImage"
 
 const Index = () => {
   return (
@@ -12,8 +13,11 @@ const Index = () => {
         <div className="flex flex-row">
           <SideNavbar />
           <MainPage>
-            <div className="text-center px-2 py-6 text-5xl font-bold text-gray-200">
-              Manchester United
+            <div className="text-center px-4 text-5xl font-bold text-gray-200">
+              <div className="bg-red-900 py-5">Manchester United</div>
+            </div>
+            <div className="p-4">
+              <MyImage />
             </div>
           </MainPage>
         </div>
---
+++ public/MyImage.js
@@ -0,0 +1,11 @@
+function MyImage() {
+  return (
+    <img
+      className="object-cover w-full"
+      src="/images/mufc_team_2020.jpg"
+      alt="MUFC Men's team 2020"
+    />
+  )
+}
+
+export default MyImage

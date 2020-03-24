ac69c6d39415b289913247c97ff9820abbb0adc7
Akshaj Verma <akshajverma7@gmail.com>
2020-03-24 20:30:18 +0530

    Export to docs directory for deployment.

---
+++ docs/out/MyImage.js
@@ -0,0 +1,13 @@
+function MyImage() {
+  return (
+    <div className="p-4 h-300 w-100">
+      <img
+        className="object-scale-down object-cover w-full"
+        src="/images/mufc_team_2020.jpg"
+        alt="MUFC Men's team 2020"
+      />
+    </div>
+  )
+}
+
+export default MyImage

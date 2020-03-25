a9afa4b2b55dff54cc7cb977c7bf014a69d2ae90
Marko Antolić <marko.antolic01@gmail.com>
2020-03-25 12:41:25 +0100

    Remove some unnecessary fragments and imports

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -2,19 +2,18 @@ import MUFCLogoImage from "./MUFCLogoImage"
 
 const SideNavbar = () => {
   return (
-    <>
-      <div className="min-h-screen w-1/6 bg-gray-200">
-        <div className="flex flex-col h-full text-center text-3xl text-gray-900 font-semibold items-center justify-around">
-          <div>
-            <MUFCLogoImage />
-          </div>
-          <div> Home </div>
-          <div> Players </div>
-          <div> Trophies </div>
-          <div> About </div>
+    <div className="min-h-screen w-1/6 bg-gray-200">
+      <div className="flex flex-col h-full text-center text-3xl text-gray-900 font-semibold items-center justify-around">
+        <div>
+          <MUFCLogoImage />
         </div>
+        {/* This should probably be a <nav></nav> element with <ul></ul> element that contains <li><a></a></li> as navigation for more semantic meaning. Also custom <Link> component from Next.js will have to be used for navigation */}
+        <div> Home </div>
+        <div> Players </div>
+        <div> Trophies </div>
+        <div> About </div>
       </div>
-    </>
+    </div>
   )
 }
 
